56aa2be8fc7bbaa7d70e4b03a78164a50a767bad
OanhNguyen <oanhnvh86@gmail.com>
2023-09-17 12:16:41 +0700

    Refresh index page

---
+++ controller/scroll.js
@@ -0,0 +1,12 @@
+
+function scrollTop() {
+    var headerr = document.getElementById("scrollTopID");
+    if (window.scrollY > 0) {
+        headerr.classList.add("changeStyle");
+    }
+    else {
+        headerr.classList.remove("changeStyle");
+    }
+}
+window.addEventListener("scroll", scrollTop);
+
